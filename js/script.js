1f6e3b4d9a2919574e63c8acf7627ea321d520fd
rodrigo7584 <rodrigo.gandhi@cojiba.com.br>
2023-03-26 23:57:04 -0300

    26/03/2023

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const btnMobile = document.querySelector('button.btn-mobile-menu')
+function toggleMenu() {
+  const navMobile = document.querySelector('header.menu nav')
+  btnMobile.classList.toggle('active')
+  navMobile.classList.toggle('active')
+}
+btnMobile.addEventListener('click', toggleMenu)
+
+const accordion = document.querySelectorAll('.accordion')
+
+accordion.forEach(item => {
+  const question = item.querySelector('.question')
+  const answer = item.querySelector('.answer')
+
+  const answerHeight = answer.scrollHeight
+  answer.style.maxHeight = 0
+
+  question.addEventListener('click', () => {
+    // answer.classList.toggle('open')
+    if (item.classList.contains('open')) {
+      answer.style.maxHeight = 0
+      item.classList.remove('open')
+    } else {
+      answer.style.maxHeight = answerHeight + 'px'
+      item.classList.add('open')
+    }
+  })
+})
